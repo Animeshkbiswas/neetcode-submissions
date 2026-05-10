59b688c84f429aded9e90298997833f73c30847d
Animesh Kumar Biswas <142120885+Animeshkbiswas@users.noreply.github.com>
2026-05-11 01:04:28 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>>mpp;
+        vector<double> time;
+        int s= speed.size();
+        int ans=s;
+
+        for(int i=0; i<speed.size();i++){
+            mpp.push_back({position[i], speed[i]});
+        }
+        sort(mpp.begin(), mpp.end());
+        for(auto it:mpp){
+            time.push_back((float)(target-(it.first))/(it.second));
+        }
+        for(int i=s-1; i>0; i--){
+            if(time[i]>=time[i-1]){
+                time[i-1]=time[i];
+                ans--;
+            }
+        }
+    return ans;
+    }
+};
